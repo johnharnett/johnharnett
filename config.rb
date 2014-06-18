0a3bc1cc726bef2d01d1ff19ba489218bb52ea51
Steve Gehrman <sgehrman@gmail.com>
2014-06-17 22:03:00 -0700

    fix

--- config.rb
+++ config.rb
@@ -72,15 +72,11 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 
-  activate :favicon_maker do |f|
-    f.template_dir  = File.join(root, 'source')
-    f.output_dir    = File.join(root, 'build')
-    f.icons = {
+  activate :favicon_maker, :icons => {
       "_favicon_template.png" => [
           { icon: "apple-touch-icon-152x152-precomposed.png" },
           { icon: "apple-touch-icon-114x114-precomposed.png" },
           { icon: "apple-touch-icon-72x72-precomposed.png" },
       ]
-    }
-    end
+  }
 end
