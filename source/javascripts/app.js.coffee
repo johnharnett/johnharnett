21a83fbf795298f7201bf95a9eae9d38eb8f8514
Steve Gehrman <sgehrman@gmail.com>
2014-06-19 03:12:22 -0700

    push state

--- source/javascripts/app.js.coffee
+++ source/javascripts/app.js.coffee
@@ -1,6 +1,5 @@
 class Coffee.AppHelper
   constructor: () ->
-    console.log("running")
     this.mainPage = $("#logo-image")
 
     this._setupLogoHover()
@@ -21,13 +20,22 @@ class Coffee.AppHelper
         scale: 1
         duration: 500
         complete: ->
+          # fade in team page
           $('#team-page').css(display: "block")
           $('#team-page').transition(opacity: 1, duration: 500)
+
+          # fade out main
           $('#main-page').transition(
             opacity: 0
             duration: 500
             complete: ->
               $('#main-page').css(display: "none")
+
+              history.pushState
+                plate_id: 2
+                plate: "team"
+              , null, "/page/team.html"
           )
       )
+
 
--- source/javascripts/push_state.js
+++ source/javascripts/push_state.js
@@ -1,41 +1,9 @@
-function burger() {
-    // URL will change to /plate/burger
-    history.pushState( {
-        plate_id: 1,
-        plate: "Burger"
-    }, null, "/plate/burger");
-
-    showPlate("Burger");
-}
-
-function sandwich() {
-    // URL will change to /plate/sandwich
-    history.pushState( {
-        plate_id: 2,
-        plate: "main"
-    }, null, "/page/main.html");
-
-    showPlate("main");
-}
-
-function fries() {
-    // URL will change to /plate/fries
-    history.pushState( {
-        plate_id: 3,
-        plate: "team"
-    }, null, "/page/team.html");
-
-    showPlate("team");
-}
-
-function showPlate(name) {
-    $("#chosen-plate").html(name);
-}
 
 window.onpopstate = function (event) {
-    var content = "";
-    if(event.state) {
+    if (event.state) {
         content = event.state.plate;
+        console.log(content);
     }
-    showPlate(content);
+
+    document.location.reload();
 }
